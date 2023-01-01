04cb2506d7db0fa46e193225982caebb631dbc76
Ahmad AlAmiri <a.alamiri.96@gmail.com>
2023-01-02 02:46:53 +0300

    checkpoint

--- src/app/food-item/food-item.component.ts
+++ src/app/food-item/food-item.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-food-item',
   templateUrl: './food-item.component.html',
-  styleUrls: ['./food-item.component.css']
+  styleUrls: ['./food-item.component.css'],
 })
 export class FoodItemComponent {
-
+  @Input() id!: number;
+  @Input() image = '';
+  @Input() name = '';
+  @Input() price!: number;
 }
--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import foodItems from 'src/assets/json/food-items.json';
+
+interface FoodItem {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
-  styleUrls: ['./item-list.component.css']
+  styleUrls: ['./item-list.component.css'],
 })
 export class ItemListComponent {
-
+  foodItems: FoodItem[] = foodItems;
+  ngOnInit() {
+    console.log(foodItems);
+  }
 }
